2a41a1d49845a592355fed38cc1dee86c09a7bc0
taizo-pro <t2s.single@gmail.com>
2020-07-26 17:44:33 +0900

    initial

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>faafafafaf</Text>
+      </View>
+    )
+  }
+}
