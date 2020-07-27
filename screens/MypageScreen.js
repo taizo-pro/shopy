fea3d077628b867bb3f4e270db31f78e5a0bb3df
taizo-pro <t2s.single@gmail.com>
2020-07-27 23:54:52 +0900

    マイページのUIを構築する

--- screens/MypageScreen.js
+++ screens/MypageScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import {Container, Header, Right, Left, Body, Title} from 'native-base';
+import {Container, Header, Right, Left, Body, Title, Content, List, ListItem} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class MypageScreen extends React.Component {
@@ -18,9 +18,50 @@ export default class MypageScreen extends React.Component {
             </TouchableOpacity>
           </Right>
         </Header>
-        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-          <Text>マイページ</Text>
-        </View>
+        <Content>
+          <List>
+            <ListItem itemDivider>
+              <Text>基本情報</Text>
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>メールアドレス変更</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>パスワード変更</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>お気に入り一覧</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem itemDivider>
+              <Text>メニュー</Text>
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>お問い合わせ</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>利用規約</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>プライバシーポリシー</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>ログアウト</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+            <ListItem itemDivider>
+            </ListItem>
+            <ListItem style={{justifyContent: 'space-between'}}>
+              <Text>アカウントを削除</Text>
+              <Icon name='chevron-right' color={'lightgrey'} size={16} />
+            </ListItem>
+          </List>
+        </Content>
       </Container>
     );
   }
