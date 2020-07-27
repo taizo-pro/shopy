11405073a3e75b45bb98172e3c237c15587df138
taizo-pro <t2s.single@gmail.com>
2020-07-27 11:56:47 +0900

    FontAwesomeを追加

--- App.js
+++ App.js
@@ -8,13 +8,26 @@ import HomeScreen from './screens/HomeScreen';
 import FavoriteScreen from './screens/FavoriteScreen';
 import MypageScreen from './screens/MypageScreen';
 
-import Icon from 'react-native-vector-icons'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          if (route.name === 'ホーム') {
+            return <Icon name='home' color={color} size={size} />
+          } else if (route.name === 'お気に入り') {
+            return <Icon name='heart' color={color} size={size} />
+          } else if (route.name === 'マイページ') {
+            return <Icon name='user' color={color} size={size} />
+          }
+        }
+      })}
+    >
       <Tab.Screen name="ホーム" component={HomeScreen} />
       <Tab.Screen name="お気に入り" component={FavoriteScreen} />
       <Tab.Screen name="マイページ" component={MypageScreen} />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,6 @@ import {
   StatusBar,
   TouchableOpacity,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 export default class HomeScreen extends React.Component {
@@ -19,9 +18,6 @@ export default class HomeScreen extends React.Component {
         <TouchableOpacity>
           <Text>ホーム画面</Text>
         </TouchableOpacity>
-        <View>
-          <Icon name='home' size={50} />
-          </View>
       </SafeAreaView>
     );
   }
