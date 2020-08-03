00c35d6220fa9b7aafd8d0879b44e3ccd0de42e5
taizo-pro <t2s.single@gmail.com>
2020-08-03 11:25:25 +0900

    メールアドレスログイン用のfirebaseAuthを実装

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,30 +1,84 @@
 import React, {Component} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import {Container, Header, Right, Left, Body, Title, Button, Content, Form, Item, Label, Input} from 'native-base';
+import {
+  Container,
+  Header,
+  Right,
+  Left,
+  Body,
+  Title,
+  Button,
+  Content,
+  Form,
+  Item,
+  Label,
+  Input,
+} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import firebase from 'firebase';
+
 export default class SignInScreen extends React.Component {
-  render() { 
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleSubmit = async () => {
+    try {
+      await firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password);
+      console.log('email login success');
+      this.props.navigation.navigate('App')
+    } catch {
+      console.log('error');
+    }
+  };
+
+  render() {
     return (
       <Container>
         <Content>
           <Form>
             <Item floatingLabel>
               <Label>メールアドレス</Label>
-              <Input />
+              <Input
+                value={this.state.email}
+                onChangeText={(input) => {
+                  this.setState({email: input});
+                }}
+              />
             </Item>
             <Item floatingLabel last>
               <Label>パスワード</Label>
-              <Input />
+              <Input
+                value={this.state.password}
+                onChangeText={(input) => {
+                  this.setState({password: input});
+                }}
+                secureTextEntry
+              />
             </Item>
           </Form>
         </Content>
         <View style={{flex: 1}}>
           <Button
-            onPress={() => this.props.navigation.navigate('App')}
-            style={{backgroundColor: 'limegreen', width: 250, alignSelf: 'center'}}
-          >
-            <Text style={{color: 'white', textAlign: 'center', flex: 1, fontSize: 18}}>はじめる</Text>
+            onPress={this.handleSubmit.bind(this)}
+            style={{
+              backgroundColor: 'limegreen',
+              width: 250,
+              alignSelf: 'center',
+            }}>
+            <Text
+              style={{
+                color: 'white',
+                textAlign: 'center',
+                flex: 1,
+                fontSize: 18,
+              }}>
+              はじめる
+            </Text>
           </Button>
         </View>
       </Container>
