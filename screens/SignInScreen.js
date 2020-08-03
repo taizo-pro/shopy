b00c0a51e1b6589b8368202bb2c8437b9b289af5
taizo-pro <t2s.single@gmail.com>
2020-08-03 11:11:50 +0900

    SignIn画面のデザイン修正

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,32 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import {Container, Header, Right, Left, Body, Title, Button} from 'native-base';
+import {Container, Header, Right, Left, Body, Title, Button, Content, Form, Item, Label, Input} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class SignInScreen extends React.Component {
   render() { 
-    console.log(this.props.navigation)
-    console.log('this.props.navigation')
     return (
       <Container>
-        {/* <Header style={{backgroundColor: 'white', marginHorizontal: 8}}>
-          <Left>
-            <Button transparent>
-              <Icon name='back' />
-              <Text>戻る</Text>
-            </Button>
-          </Left>
-          <Body>
-            <Title>shopyをはじめましょう</Title>
-          </Body>
-          <Right />
-        </Header> */}
-
-        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>メールアドレス</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>パスワード</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+        <View style={{flex: 1}}>
           <Button
             onPress={() => this.props.navigation.navigate('App')}
+            style={{backgroundColor: 'limegreen', width: 250, alignSelf: 'center'}}
           >
-            <Text>はじめる</Text>
+            <Text style={{color: 'white', textAlign: 'center', flex: 1, fontSize: 18}}>はじめる</Text>
           </Button>
         </View>
       </Container>
