5016b09cc1e2f73dede74f29b634f9c2d65b66b7
taizo-pro <t2s.single@gmail.com>
2020-08-09 13:22:45 +0900

    Facebookログインボタン作成

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -17,11 +17,52 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import firebase from 'firebase';
+import auth from '@react-native-firebase/auth';
+import {LoginManager, AccessToken} from 'react-native-fbsdk';
 
 export default class SignUpScreen extends React.Component {
   state = {
     email: '',
     password: '',
+  };
+
+  facebookSignUp = () => {
+    return (
+      <Button
+        title="Facebook"
+        onPress={() =>
+          onFacebookButtonPress().then(() => {
+            console.log('Signed in with Facebook!');
+          })
+        }
+      />
+    );
+  };
+
+  onFacebookButtonPress = async () => {
+    const result = await LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+    ]);
+
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+
+    // 一度ログインしているユーザー
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(
+      data.accessToken,
+    );
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
   };
 
   handleSubmit = async () => {
@@ -30,7 +71,7 @@ export default class SignUpScreen extends React.Component {
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password);
       console.log('email login success');
-      this.props.navigation.navigate('App')
+      this.props.navigation.navigate('App');
     } catch {
       console.log('error');
     }
@@ -80,14 +121,24 @@ export default class SignUpScreen extends React.Component {
               はじめる
             </Text>
           </Button>
-        <View style={{flexDirection: 'row', alignSelf: 'center', margin: 30}}>
-          <Text>登録済みですか？</Text>
-          <TouchableOpacity
-            onPress={() => {this.props.navigation.navigate('SignIn')}}
-          >
-            <Text style={{color: 'deepskyblue'}}>ログイン</Text>
-          </TouchableOpacity>
-        </View>
+          <View style={{flexDirection: 'row', alignSelf: 'center', margin: 30}}>
+            <Button
+            style={{width: 200}}
+              title="Facebook"
+              onPress={() =>
+                this.onFacebookButtonPress().then(() => {
+                  console.log('Signed in with Facebook!');
+                })
+              }
+            />
+            <Text>登録済みですか？</Text>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.navigation.navigate('SignIn');
+              }}>
+              <Text style={{color: 'deepskyblue'}}>ログイン</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </Container>
     );
